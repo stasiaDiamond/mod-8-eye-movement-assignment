88a3a866b29401e362efd09730b596eaef9bc4ec
Nastasia <nsisco@uw.edu>
2023-12-02 13:58:30 -0800

    adds file structure  per portfolio project instructons

--- eyes.js
+++ eyes.js
@@ -4,12 +4,12 @@ document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  // *first eye
+  // * first eye
   balls[0].style.left = x;
   balls[0].style.top = y;
   balls[0].transform = 'translate(-' + x + ',-' + y + ')';
 
-  // *second eye
+  // * second eye: only thing i've done 
   balls[1].style.left = x;
   balls[1].style.top = y;
   balls[1].transform = 'translate(-' + x + ',-' + y + ')';
